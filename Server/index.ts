425682ecc081ae6195a23c13cb262d18784be6a2
DerpyCrabs <derpycrabs@gmail.com>
2024-11-17 16:31:27 +0300

    Remove shouldUpdate and add better press logging

--- Server/index.ts
+++ Server/index.ts
@@ -2,30 +2,28 @@ import { TextDatum, WidgetType, getWidgetsPayload, type Widget } from './widgets
 
 class TodoApp {
   count: number = 0
-  widgets: Widget[] = [
-    { widgetType: WidgetType.Button, id: 'myButton', x: 100, y: 200, w: 300, h: 150 },
-    {
-      widgetType: WidgetType.Label,
-      id: 'myLabel',
-      x: 250,
-      y: 275,
-      datum: TextDatum.MiddleCenter,
-      fontSize: 3,
-      text: 'Test button',
-    },
-    { widgetType: WidgetType.Line, x1: 0, y1: 500, x2: 540, y2: 500 },
-  ]
 
   constructor() {}
 
   getWidgets(): Widget[] {
-    return this.widgets.map((w) => (w.id === 'myLabel' ? { ...w, text: `Count ${this.count}` } : w))
+    return [
+      { widgetType: WidgetType.Button, id: 'myButton', x: 100, y: 200, w: 300, h: 150 },
+      {
+        widgetType: WidgetType.Label,
+        id: 'myLabel',
+        x: 250,
+        y: 275,
+        datum: TextDatum.MiddleCenter,
+        fontSize: 3,
+        text: `Count ${this.count}`,
+      },
+      { widgetType: WidgetType.Line, x1: 0, y1: 500, x2: 540, y2: 500 },
+    ]
   }
-  reactToTouch(pressedAreaId?: string): boolean {
+  reactToTouch(pressedAreaId?: string) {
     if (pressedAreaId === 'myButton') {
       this.count += 1
     }
-    return true
   }
 }
 
@@ -44,16 +42,16 @@ Bun.serve({
       const [x, y] = [Number(parts[0]), Number(parts[1])]
 
       const widgets = app.getWidgets()
-      let shouldUpdate = false
+      let reactionCount = 0
       widgets.forEach((w) => {
         if (w.widgetType === WidgetType.Button && x >= w.x && x <= w.x + w.w && y >= w.y && y <= w.y + w.h) {
-          shouldUpdate = app.reactToTouch(w.id) || shouldUpdate
+          app.reactToTouch(w.id)
+          reactionCount += 1
         }
       })
 
-      // TODO shouldUpdate = false shouldn't send new widgets
       const payload = getWidgetsPayload(app.getWidgets())
-      console.log(`POST request: sent ${widgets.length} widgets`)
+      console.log(`POST request: sent ${widgets.length} widgets, detected press on ${reactionCount}`)
       return new Response(payload)
     }
   },
